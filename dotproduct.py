d50d982a0c8810b396c57c17aa0d1adefb72487d
Liqun Li <liqlee@gmail.com>
2025-07-28 15:23:13 -0700

    add dot product Python3 code

---
+++ dotproduct.py
@@ -0,0 +1,67 @@
+'''
+ run by :
+ $ python3 dotproduct.py
+'''
+import numpy as np
+import random as rd
+import time
+import psutil
+import os
+
+def get_memory_usage():
+  memory_info = psutil.virtual_memory()
+  print(f"{memory_info.available / (1024**3):.2f} GB  ", f"{(100-memory_info.percent) :.2f}", '%  ', f"{memory_info.total / (1024**3):.2f} GB  ")
+
+
+LEN = 10000000
+
+def dotProduct (arr, brr) :
+  #print(f' LEN : {LEN}  dot product')
+  result = 0.0
+  for i in range(LEN) :
+    result += arr[i] * brr[i]
+  return result
+
+def get_process_memory():
+  print(' os.getpid() : ', os.getpid())
+  process = psutil.Process(os.getpid())
+  mem_info = process.memory_info()
+  return mem_info.rss
+
+if __name__ == "__main__":
+  #get_memory_usage()
+  arr, brr = [], []
+  print(' numpy version : ', np.__version__)
+
+  #memRss = get_process_memory()
+  #print(" memRss : {:,} ".format( memRss))
+
+  #get_memory_usage()
+  print(f' LEN : {LEN}  init two arrays, ')
+  start = time.time()
+  for i in range(LEN) :
+    arr.append(rd.random())
+    brr.append(rd.random())
+  end = time.time()
+  #get_memory_usage()
+  #print(f' python two arrays init time {int(1000 * (end - start))} ms, start : {start} ms, end : {end} ms ')
+  print(f' python two arrays init time {int(1000 * (end - start))} ms,  ')
+
+  #memRss = get_process_memory()
+  #print(" memRss : {:,} ".format( memRss))
+  start = time.time()
+  res = dotProduct(arr, brr)
+  end = time.time()
+  #get_memory_usage()
+  #print(f' python time {int(1000 * (end - start))} ms, start : {start} ms, end : {end} ms. result : {res} ')
+  print(f' python time {int(1000 * (end - start))} ms,  ')
+
+  start = time.time()
+  res = np.dot(arr, brr)
+  end = time.time()
+  #get_memory_usage()
+  #print(f' numpy time {int(1000 * (end - start))} ms, start : {start} ms, end : {end} ms.  result : {res} ')
+  print(f' numpy time {int(1000 * (end - start))} ms,  ')
+
+
+ 
